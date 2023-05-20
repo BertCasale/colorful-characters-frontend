b31cde7ea0ac1abf1a4855174326979f23855c17
BertCasale <bthebest55@gmail.com>
2023-05-20 10:24:53 -0400

    show characters on game details

--- src/Components/GameDetails.js
+++ src/Components/GameDetails.js
@@ -7,11 +7,20 @@ export default function GameDetails() {
   const navigate = useNavigate();
   const {id} = useParams();
   const [game, setGame] = useState({})
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     axios.get(`${API}/games/${id}`)
     .then((res) => {
       setGame(res.data)
+    })
+    .catch((e) => console.warn("catch", e))
+  }, [id]);
+
+  useEffect(() => {
+    axios.get(`${API}/games/${id}/characters`)
+    .then((res) => {
+      setCharacters(res.data)
     })
     .catch((e) => console.warn("catch", e))
   }, [id]);
@@ -30,7 +39,7 @@ export default function GameDetails() {
 
   return(<div className="GamesDetails">
     <h2>{game.name}</h2>
-    <h3>From: {game.release}</h3>
+    <h3>Release: {game.release}</h3>
     <h4>{game.platforms}</h4>
     <img src={game.image} alt={game.name} width="400px"/>
     <p>{game.description}</p>
@@ -39,7 +48,7 @@ export default function GameDetails() {
       <h5>Person of Color:{game.poc ? " ⭐️" : " ❌"}</h5>
       <h5>Disabled:{game.disability ? " ⭐️"  : " ❌"}</h5>
     </span>
-    
+
     <Link to={`/games/${id}/edit`}><button>Edit</button></Link>
     <button data-bs-toggle="modal" data-bs-target="#deleteModal">Delete</button>
     <Link to="/games"><button>Back to Games</button></Link>
@@ -60,6 +69,35 @@ export default function GameDetails() {
 
         </div>
       </div>
+    </div>
+
+    <div className="game-characters">
+      <h3>Characters</h3>
+      <Link to="/characters/new"><button>New Character</button></Link>
+
+      {characters[0] ? 
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>LGBTQ</th>
+            <th>Person of Color</th>
+            <th>Disabled</th>
+          </tr>
+        </thead>
+        <tbody>
+          {characters.map((character) => {
+            return (<tr key={character.id} className="table-info">
+              <td><Link to={`/characters/${character.id}`}>{character.name}</Link></td>
+              <td>{character.lgbt ? character.lgbt_type : "❌"}</td>
+              <td>{character.poc ? character.poc_type : "❌"}</td>
+              <td>{character.disability ? character.disability_type : "❌"}</td>
+            </tr>)
+          })}
+        </tbody>
+        
+      </table>
+      : "Please add a character for this game."}
     </div>
 
   </div>);
